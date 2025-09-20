7d9ae431822c9c38d5fb0e8cb7ca5bfd5af15d87
mohamedballa <sudanfutuer@gmail.com>
2025-09-20 19:53:04 +0400

    Testing

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home(){
     return (
-        <h1 className=" bg-red-400 w-full sm:w-1/4 md:w-1/2 lg:w-3/4">
+        <h1 className=" bg-red-900 w-full sm:w-1/4 md:w-1/2 lg:w-3/4">
   Hello World
 </h1>
 
