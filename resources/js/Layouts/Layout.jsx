29ed644b9ca6b76ba158ed949b0684f1cbea07ed
mohamedballa <sudanfutuer@gmail.com>
2025-09-23 18:19:42 +0400

    Add svgr and imported logo and svg

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,4 +1,7 @@
 import { Link } from "@inertiajs/react";
+import Logo from '../assets/images/logo.png';
+import Icon  from '../assets/icons/ei_arrow-right.svg?react';
+
 
 export default function Layout({children}){
     return (
@@ -8,7 +11,8 @@ export default function Layout({children}){
                     <div>
                         
                         <div>
-                            {/* <img src="" alt="" /> */}
+                            <img src={Logo} alt="Logo" className="" />
+                            <Icon className="w-9 text-white bg-primary" />
                         </div>
                         <div>
                            <Link href="/">Home</Link>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import svgr from  'vite-plugin-svgr';
+
 
 export default defineConfig({
     plugins: [
@@ -9,6 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        svgr(),
     ],
     server: {
         hmr: {
