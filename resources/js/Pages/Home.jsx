7d259c371b73269b73c8498b6a24c9237950124f
mohamedballa <sudanfutuer@gmail.com>
2025-09-23 21:54:29 +0400

    Add Hero in svg form, and test, very good

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,20 @@
 import {usePage , Head, Link } from "@inertiajs/react";
-
+import Hero  from '../assets/icons/Hero-large.svg?react';
 
 
 export default function Home() {
     
     const component =  usePage();
     return (
-        <>   
-        <Head  title="Home"  />
-       
-        <h1 className=" bg-red-300 text-9xl  w-full sm:w-1/4 md:w-1/2 lg:w-3/4 ">
-            Home
-        </h1>
+        <> 
+        <div className="bg-white">
+            <Head  title="Home"  />
+            <img src={Hero}  className="" />
+            <Hero className=" max-w-full  " />
+            <h1 className=" bg-red-300 text-9xl  w-full sm:w-1/4 md:w-1/2 lg:w-3/4 ">
+                Home
+            </h1>
+        </div>  
         </>
     );
 }
